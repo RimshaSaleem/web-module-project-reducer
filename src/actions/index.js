24b2c0aed5e9163c50ed648febfa46e94df2783e
RimshaSaleem <rimshatif8@gmail.com>
2021-08-09 20:33:05 -0700

    add new actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,15 +1,27 @@
 export const ADD_ONE = "ADD_ONE";
-
 export const APPLY_NUMBER = "APPLY_NUMBER";
 export const CHANGE_OPERATION = "CHANGE_OPERATION";
+export const CLEAR_DISPLAY = "CLEAR_DISPLAY";
+export const MEMORY_CLEAR = "MEMORY_CLEAR";
+export const MEMORY_PLUS = "MEMORY_PLUS";
 
 export const addOne = (number) => {
     return({type:ADD_ONE, payload:number});
 }
 
-export const applyNumber = (number) => {
-    return({type:APPLY_NUMBER, payload:number});
+export const applyNumber = () => {
+    return({type:APPLY_NUMBER});
 }
 export const changeOperator = (number) => {
     return({type:CHANGE_OPERATION, payload:number});
 }
+export const clearDisplay = () => {
+    return { type: CLEAR_DISPLAY };
+
+};
+export const memoryClear = () => {
+    return { type: MEMORY_CLEAR };
+};
+export const memoryPlus = (total) => {
+    return { type: MEMORY_PLUS, payload: total };
+};
