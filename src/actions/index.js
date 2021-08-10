557ebe04bdf4b220b58d1564db358ef76f259a19
RimshaSaleem <rimshatif8@gmail.com>
2021-08-09 20:38:02 -0700

    wip

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,4 @@
-export const ADD_ONE = "ADD_ONE";
+
 export const APPLY_NUMBER = "APPLY_NUMBER";
 export const CHANGE_OPERATION = "CHANGE_OPERATION";
 export const CLEAR_DISPLAY = "CLEAR_DISPLAY";
@@ -6,9 +6,6 @@ export const MEMORY_CLEAR = "MEMORY_CLEAR";
 export const MEMORY_PLUS = "MEMORY_PLUS";
 export const MEMORY_RECALL = "MEMORY_RECALL";
 
-export const addOne = (number) => {
-    return({type:ADD_ONE, payload:number});
-}
 
 export const applyNumber = (number) => {
     return({type:APPLY_NUMBER, payload:number});
