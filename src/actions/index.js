a4bc16e1faa4041dddbded8a2d90866d5def0c8a
RimshaSaleem <rimshatif8@gmail.com>
2021-08-09 20:28:45 -0700

    wip

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,10 +3,13 @@ export const ADD_ONE = "ADD_ONE";
 export const APPLY_NUMBER = "APPLY_NUMBER";
 export const CHANGE_OPERATION = "CHANGE_OPERATION";
 
-export const addOne = () => {
-    return({type:ADD_ONE});
+export const addOne = (number) => {
+    return({type:ADD_ONE, payload:number});
 }
 
 export const applyNumber = (number) => {
     return({type:APPLY_NUMBER, payload:number});
+}
+export const changeOperator = (number) => {
+    return({type:CHANGE_OPERATION, payload:number});
 }
